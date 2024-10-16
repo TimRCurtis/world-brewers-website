cd3739348e58828ffc118dd682935b227a96a979
Tim Curtis <timrcurtis@gmail.com>
2024-10-15 20:57:42 -0400

    updated nuxt.config with head metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'World Brewers Website',
+      meta: [
+        { charset: 'utf-8' },
+        { "http-equiv": "X-UA-Compatible", content: "IE=edge" },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'Tim Curtis Designs portfolio page displaying my work and a little info on who I am.' },
+      ],
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap' },
+      ],
+    },
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   ssr: true,
